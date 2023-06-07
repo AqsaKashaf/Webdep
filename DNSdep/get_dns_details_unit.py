5d350bcbb41b08452aecf3f2fe03d6b5b9aa838a
AqsaKashaf <akashaf@andrew.cmu.edu>
2023-06-07 13:00:29 +0500

    dns error handling

--- DNSdep/get_dns_details_unit.py
+++ DNSdep/get_dns_details_unit.py
@@ -17,7 +17,7 @@ from config import *
 from collections import defaultdict
 log = logging.getLogger(__name__)
 
-def find_if_dns_third(website, ns, concentration, soa_w=None, soa_p=None):
+def find_if_dns_third(website, ns, concentration, soa_w=None, soa_p=None, san_w=None):
 
     third = "unknown"
     if(match_TLD(website,ns)):
@@ -32,7 +32,7 @@ def find_if_dns_third(website, ns, concentration, soa_w=None, soa_p=None):
     if("awsdns" in ns or "ultradns" in ns or "cloudflare" in ns or "domaincontrol" in ns or "dnsv1" in ns or "dnsv2" in ns  or "dnsv3" in ns  or "dnsv4" in ns  or "dnsv5" in ns or "arvancdn" in ns or "xserver.jp" in ns or "webedia-group" in ns or "comlaude-dns" in ns or "nscluster" in ns or "easydns" in ns or "nominalia" in ns or "mydnscloud.com" in ns or "dtag.de" in ns or "alphadnszone.com" in ns or "aziondns" in ns or "arubadns" in ns or "hostdns.co.za" in ns or "first-ns.de" in ns or "mybluehost" in ns or "dnscpanel" in ns or "hostserv.co.za" in ns or "easydns" in ns or "dnsv.jp" in ns or "oraclecloud.net" in ns or "crystalwebhosting" in ns or "bestwebhosting" in ns or "arvancdn" in ns or "reflected.net" in ns or "comlaude" in ns or "nameserver2.co.za" in ns or "nameserver1.co.za" in ns or "nameserver3.co.za" in ns or "nameserver4.co.za" in ns or "managed-ns" in ns or "transip" in ns or "edgecastdns.net" in ns and "1and1-dns" in ns or"eurodns" in ns or "clustereddns" in ns or "zsd.co.za" in ns or "zomerlust" in ns or "turtle.co.za" in ns or "enetworks" in ns or "iewc.co.za" in ns or "clusterdns" in ns or "easyweb.co.za" in ns or "regzone" in ns or "brilliantweb.co.za" in ns or "aasaam.net" in ns or "x-ns.com" in ns or "x-ns.it" in ns or "srv53.net" in ns or "novagraaf" in ns or "naver" in ns or "netuse.de" in ns or "infoedgeindia.net" in ns or "hostdl.com" in ns or "gehirndns" in ns or "gabia" in ns or "d-53." in ns or "active24." in ns or "hichina" in ns or "second-ns." in ns or "esicia.rw" in ns or "kaneza.com" in ns or "liquidtelecom.rw" in ns or "mtnonline.rw" in ns or "afriregister.com" in ns or "beget.pro" in ns or "beget.com" in ns or "contabo.net" in ns or "cscdns.uk" in ns or "dns-h.com" in ns or "host-h.net" in ns or "inhostedns.com" in ns or "liquidweb.com" in ns or "liquidtelecom.net" in ns or "zubahost.com" in ns or "observatoiredesmarques.fr" in ns or "rackspace.com" in ns or "stackdns.com" in ns or "tigertech." in ns or "edrive" in ns or "reg.ru" in ns or "register.it" in ns or "hsmedia.ru" in ns or "perf1.fr" in ns or "rackforest.hu" in ns or "msgafrica.com" in ns or "jtl.co.ke" in ns or "xtranet.co.ke" in ns or "itexpertskenya.co.ke" in ns or "is.co.ke" in ns or "iskenya.co.ke" in ns or "myisp.co.ke" in ns or "jamii.co.ke" in ns or "netimcloud.co.ke" in ns or "bluewebsafrica.co.ke" in ns or "fastly.net" in ns or "gandi.net" in ns or "stackpathdns.net" in ns or "anycast.me" in ns or"ovh.net" in ns or "cdnetdns" in ns or "mojohost.com" in ns or "hover.com" in ns or "townnews.com" in ns or "azure-dns" in ns):
         return "Third"
     
-    if(inSAN(website,ns)):
+    if(inSAN(website,ns, san_w)):
         return "Pvt"
 
     if(not soa_w): soa_w = get_SOA(website)
@@ -166,12 +166,12 @@ def read_DNS_concentration():
         conc[ns] += 1
     return conc
 
-def classify(website,ns,soa_w=None, soa_p=None):
+def classify(website,ns,soa_w=None, soa_p=None, san_w=None):
 
     
     output = "unknown"
     concentration = read_DNS_concentration()
-    output = find_if_dns_third(website, ns, concentration, soa_w,soa_p)
+    output = find_if_dns_third(website, ns, concentration, soa_w,soa_p, san_w)
     
     return output
 
@@ -196,12 +196,13 @@ def find_and_classify(host: str) -> tuple:
     name_servers = get_DNS_details(host)
     soa_ns = {}
     soa_w = get_SOA(host)
+    san_w = get_san(host)
     ns_type = {}
     third = 0
     for ns in name_servers:
         if(ns not in soa_ns):
             soa_ns[ns] = get_SOA(ns)
-        ns_type[ns] = classify(host, ns, soa_w, soa_ns[ns])
+        ns_type[ns] = classify(host, ns, soa_w, soa_ns[ns], san_w)
         if(ns_type[ns] == "Third"):
             third+=1
     ns_groups = {}
